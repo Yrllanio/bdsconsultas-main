4c033005368ecd734ccabf5ca94bd2f5286f60d5
Yrllanio <yrllanio.souza@tinnova.com.br>
2022-10-06 18:48:05 -0300

    Finish SQL RAIZ and JPQL

--- bdsconsultas-main/uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ bdsconsultas-main/uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,25 @@
 package com.devsuperior.uri2609.repositories;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2609.entities.Category;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query(nativeQuery = true, value = "select c.name , sum(p.amount) " +
+            "from categories c " +
+            "inner join products p ON p.id_categories = c.id " +
+            "group by c.name")
+    List<CategorySumProjection> search1();
+
+    @Query("select new com.devsuperior.uri2609.dto.CategorySumDTO(obj.category.name, sum(obj.amount)) " +
+            "from Product obj " +
+            "group by obj.category.name")
+    List<CategorySumDTO> search2();
 
 }
--- bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,9 +1,22 @@
 package com.devsuperior.uri2737.repositories;
 
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2737.entities.Lawyer;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
+
+    @Query(nativeQuery = true, value = "(select l.name , l.customers_number as customersNumber from lawyers l where l.customers_number = ( " +
+            "select max(l2.customers_number) from lawyers l2)) " +
+            "union all " +
+            "(select l.name , l.customers_number  from lawyers l where l.customers_number = ( " +
+            "select min(l2.customers_number) from lawyers l2)) " +
+            "union all " +
+            "(select 'Average', round(AVG(l.customers_number),0) from lawyers l)")
+    List<LawyerMinProjection> search1();
 
 }
